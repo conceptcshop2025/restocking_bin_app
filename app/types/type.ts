17d4ca8bcc7567e03f072ca63bba88cfd19dea32
Felipe Cancino <jf.cancino@conceptcshop.com>
2025-12-18 15:10:31 -0500

    refactor editable input qty product

--- app/types/type.ts
+++ app/types/type.ts
@@ -49,8 +49,3 @@ export type BinValidatorProps = {
   bAlias: string[] | [];
   onValidate?: (item:HTMLElement, successItem:boolean) => void;
 }
-
-export type QtyToRestockProps = {
-  upc: string;
-  qty: number;
-}
