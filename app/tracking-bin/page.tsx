c2e23ae2f1814f656203d2ea4b5067eb965c0112
Felipe Cancino <jf.cancino@conceptcshop.com>
2026-01-07 13:04:48 -0500

    add function complete data to get all product data

--- app/tracking-bin/page.tsx
+++ app/tracking-bin/page.tsx
@@ -3,13 +3,13 @@
 import Link from "next/link";
 import Image from "next/image";
 import { ArrowUturnLeftIcon } from "@heroicons/react/16/solid";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import type { ProductSold } from "../types/type";
 import BinStatus from "../components/BinStatus/BinStatus";
+import pLimit from "p-limit";
 
 export default function TrackingBinPage() {
-  const appVersion = "1.0.0";
-
+  const appVersion = "1.1.0";
   const [productSoldList, setProductSoldList] = useState<ProductSold[]>([]);
 
   async function getData() {
@@ -20,8 +20,8 @@ export default function TrackingBinPage() {
         throw new Error(`Error fetching data: ${response.statusText}`);
       }
       const data = await response.json();
-      console.log("Fetched data:", data);
-      setProductSoldList(data.data.rows.map((row: any) => ({
+
+      const formattedList: ProductSold[] = data.data.rows.map((row: any) => ({
         name: row.product_title,
         type: row.product_type,
         sku: row.product_variant_sku,
@@ -30,10 +30,59 @@ export default function TrackingBinPage() {
         binLocation: '',
         htsus: '',
         imageUrl: ''
-      })));
+      }));
+
+      setProductSoldList(formattedList);
+
+      await completeData(formattedList); 
+      
     } catch (error) {
       console.error("Error during fetch:", error);
     }
+  }
+
+  async function completeData(itemsToProcess: ProductSold[]) {
+    const limit = pLimit(5);
+
+    const promises = itemsToProcess.map((row: ProductSold) => {
+      return limit(async () => {
+        const sku = row.sku;
+
+        if (!sku) return;
+
+        try {
+          const baseUrl = `/api/ipacky?code=${sku}&type=sku`;
+          const response = await fetch(baseUrl);
+          const result = await response.json();
+
+          if (response.ok && result.data && result.data.length > 0) {
+            const productData = result.data[0];
+
+            setProductSoldList(prevList =>
+              prevList.map(product =>
+                product.sku === row.sku
+                  ? {
+                      ...product,
+                      upc: productData.barcode || '',
+                      binLocation: productData.binLocations || [],
+                      htsus: productData.htsUS || null,
+                      imageUrl: productData.imageURL || ''
+                    }
+                  : product
+              )
+            );
+
+          } else {
+            console.error(`Error: SKU: ${sku} not finded or API's error`);
+          }
+        } catch (error) {
+          console.error(error);
+          console.error(`Critical Error: SKU: ${sku}`);
+        }
+      });
+    });
+
+    await Promise.all(promises);
   }
 
   return (
@@ -60,7 +109,7 @@ export default function TrackingBinPage() {
       </section>
       <div className="container flex justify-center w-full mx-auto mb-4">
         <button className="bg-sky-500 py-2 px-4 rounded-lg flex items-center justify-center gap-4 hover:bg-sky-700 ease-in-out duration-300 cursor-pointer text-white" onClick={() => getData()}>
-          Get Data !
+          Obtenir ventes de la journée précédente
         </button>
       </div>
 
@@ -100,21 +149,34 @@ export default function TrackingBinPage() {
                       </span>
                     </td>
                     <td className="text-center">
-                      <span>{ product.htsus }</span>
+                      <span className={`${!product.htsus && 'text-red-500'}`}>{ product.htsus ? product.htsus : "inconnu" }</span>
                     </td>
                     <td className="text-center">
                       <span>{ product.soldQuantity }</span>
                     </td>
                     <td className="text-center">
-                      <span>--</span>
+                      <span className={`${!product.htsus && 'text-red-500'}`}>
+                        {
+                          product.htsus && product.soldQuantity ? `${ Number(product.htsus) - Number(product.soldQuantity) } (${ Math.round((Number(product.htsus) - Number(product.soldQuantity)) / Number(product.htsus) * 100) }%)` : "inconnu"
+                        }
+                      </span>
                     </td>
                     <td className="text-center">
-                      <span>-</span>
+                      <div className="flex flex-col gap-2 py-2">
+                        {
+                          Array.isArray(product.binLocation) ?
+                          product.binLocation.map((bin, idx) => (
+                            <span key={idx} className="p-2 bg-[#e4e5e7] inline-block h-fit rounded-md font-sans">{ bin }</span>
+                          ))
+                          :
+                          <span>{ product.binLocation }</span>
+                        }
+                      </div>
                     </td>
                     <td className="text-center">
                       <BinStatus
                         qty={Number(product.soldQuantity)}
-                        maxQty={100} />
+                        maxQty={Number(product.htsus)} />
                     </td>
                   </tr>
                 ))
